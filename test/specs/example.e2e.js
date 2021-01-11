58777c9f813e7bcc618aac72e51dd56f233e9b25
RajaKing <raja.venuchetty@gmail.com>
2021-01-10 20:28:43 -0800

    First Commit

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,17 @@
+const LoginPage = require('../pageobjects/login.page');
+const SecurePage = require('../pageobjects/secure.page');
+
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        LoginPage.open();
+        browser.pause(5000);
+
+        LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        expect(SecurePage.flashAlert).toBeExisting();
+        expect(SecurePage.flashAlert).isDisplayed();
+        expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
+
